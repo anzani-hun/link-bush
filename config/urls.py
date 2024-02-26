37bbd2d192b7e61fd4d59c59a0396542759d0a39
anzani-hun <anzani@hun>
2024-02-26 18:29:30 +0100

    harmadik mentés

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login_page, name ='login'),
+    path('', views.index, name='index'),
 ]
--- config/views.py
+++ config/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
 
 def login_page(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            
+            user = authenticate(
+                username = form.cleaned_data.get('username'),
+                password = form.cleaned_data.get('password')
+            )
+        
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+
+    login_form = AuthenticationForm()
+
+    return render(request, 'login.html',
+                    {"form" : login_form})
